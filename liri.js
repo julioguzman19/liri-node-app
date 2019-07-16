c06ff1d9b36bab3d3d9fcb0ee300208acedbeda6
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-07-15 22:58:42 -0600

    initializing files

---
+++ liri.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+let keys = require("./keys.js");
+let command = process.argv[2];
+
+if(command === "concert-this"){
+
+}
+if(command === "spotify-this-song"){
+    
+}
+if(command === "movie-this"){
+    
+}
+if(command === "do-what-it-says"){
+    
+}
