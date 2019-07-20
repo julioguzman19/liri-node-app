858806aab714e9e6b0c8475bdbc32705f814eaf9
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-07-19 20:49:22 -0600

    OMDB failing

--- liri.js
+++ liri.js
@@ -4,8 +4,15 @@ require("dotenv").config();
 
 //
 
+// TODO Grab the axios package...
+// @link https://www.npmjs.com/package/axios
+var axios = require("axios");
+
+
 const keys = require("./keys.js");
 let command = process.argv[2];
+
+
 /* 
 process.env
 if(command === "concert-this"){
@@ -53,14 +60,12 @@ Venue location
 Date of the Event (use moment to format this as "MM/DD/YYYY")
 */
 
-// TODO Grab the axios package...
-// @link https://www.npmjs.com/package/axios
-var axios = require("axios");
+
 
 // Run the axios.get function...
 // The axios.get function takes in a URL and returns a promise (just like $.ajax)
 
-let queryURL = "https://rest.bandsintown.com/artists/" + "Marshmellow" + "/events?app_id="
+/* let queryURL = "https://rest.bandsintown.com/artists/" + "Marshmellow" + "/events?app_id="
 //example: https://rest.bandsintown.com/artists/Marshmellow/events?app_id=
 axios.get("queryURL").then(
   function(response) {
@@ -86,4 +91,36 @@ axios.get("queryURL").then(
     }
     console.log(error.config);
   }
-);
+); */
+
+
+  /*---------OMDB Movies---------- */
+  let queryURL = "http://www.omdbapi.com/t=" + "Batman" + "?apikey="
+  //example: http://www.omdbapi.com/?apikey=[yourkey]&   using t= as parameter
+  axios
+  .get("queryURL")
+  .then(
+    function(response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data); 
+    },
+  
+    function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    }
+  );
