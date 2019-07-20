3e80209d29b6594f850a2b7d20a7cbf9d9d93f4d
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-07-19 18:55:52 -0600

    Spotify artist, song, album, link success

--- liri.js
+++ liri.js
@@ -34,10 +34,10 @@ spotify
   .search({ type: 'track', query: 'All the Small Things' })
   .then(function(response) {
     //navigate through object to Artist, song name (which i have), preview link of song from spotify, album of song
-    console.log(response.tracks.items[0].name);
-    /* console.log(response.tracks.items[0].name);
-    console.log(response.tracks.items[0].name);
-    console.log(response.tracks.items[0].name); */
+    console.log(response.tracks.items[0].album.artists[0].name); //artist name
+    console.log(response.tracks.items[0].name); //song name
+    console.log(response.tracks.items[0].album.name); //album name
+    console.log(response.tracks.items[0].external_urls.spotify); //preview link
   })
   .catch(function(err) {
     console.log(err);
